6852594115888c690fb911b92ecde646732b431c
Nadhil-an <nadhil.edu@gmail.com>
2025-08-27 05:26:12 +0530

    create super user

--- foodonline_main/foodonline_main/views.py
+++ foodonline_main/foodonline_main/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('hello world')
+    return render(request,'home.html')
 
-def about(request):
-    return HttpResponse("about page")
