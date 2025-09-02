d6be4536e63e923340b9676ba011ebe9ad5e76ae
Nadhil-an <nadhil.edu@gmail.com>
2025-09-02 23:09:44 +0530

    setuped home page

--- foodonline_main/foodonline_main/settings.py
+++ foodonline_main/foodonline_main/settings.py
@@ -114,11 +114,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT =BASE_DIR/'static'
+STATIC_URL = '/static/'
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 STATICFILES_DIRS = [
-    'foodonline_main/static'
+    BASE_DIR / "foodonline_main" / "static",
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
