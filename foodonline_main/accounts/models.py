3341667b3ca79dc44e8fa03688d934a90ef04cbe
Nadhil-an <nadhil.edu@gmail.com>
2025-09-07 17:04:10 +0530

    django signal to create user profile

--- foodonline_main/accounts/models.py
+++ foodonline_main/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 
+
 # Create your models here.
 class userManager(BaseUserManager):
     def create_user(self,first_name,last_name,username,email,password=None):
@@ -83,6 +84,7 @@ class User(AbstractBaseUser):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=20,blank=True,null=True)
     profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
     cover_photo = models.ImageField(upload_to='users/cover_photos',blank=True,null=True)
     address_line_1 = models.CharField(max_length=50,blank=True,null=True)
