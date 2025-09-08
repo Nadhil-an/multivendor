5ed5074797017622d7024c193b92ab028f338a6e
Nadhil-an <nadhil.edu@gmail.com>
2025-09-09 02:29:46 +0530

    user registeration path

--- foodonline_main/accounts/models.py
+++ foodonline_main/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
-
+  
 
 # Create your models here.
 class userManager(BaseUserManager):
@@ -16,7 +16,7 @@ class userManager(BaseUserManager):
             first_name = first_name,
             last_name  = last_name,
         )
-
+ 
         user.set_password(password)
         user.save(using=self._db)
         return user
