8854279c317985d0aa303f25581218a505258c68
Nadhil-an <nadhil.edu@gmail.com>
2025-09-10 00:53:12 +0530

    user registration form implementation

--- foodonline_main/accounts/views.py
+++ foodonline_main/accounts/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from . forms import UserForm
+from . models import User
+
 # Create your views here.
 
 
 def registerUser(request):
-    return HttpResponse('this is a test')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER       
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('registerUser')  # reload same page
+    else:
+        form = UserForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/userRegister.html', context)
