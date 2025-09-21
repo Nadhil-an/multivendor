81966fa61a909f26df13a60379fc1c90a668ed43
Nadhil-an <nadhil.edu@gmail.com>
2025-09-21 20:25:54 +0530

    complete the account activation functionality

--- foodonline_main/accounts/views.py
+++ foodonline_main/accounts/views.py
@@ -7,6 +7,8 @@ from . models import User
 from vendor.forms import VendorForm
 from .models import UserProfile
 from . utilis import detectUser,send_verification_email
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 
 
@@ -146,7 +148,7 @@ def logout(request):
 # -----------------------------
 # Dashboard Redirection
 # -----------------------------  
-@login_required(login_url='login')
+@login_required(login_url='loginUser')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     return render(request, 'accounts/vendorDashboard.html')
@@ -159,7 +161,7 @@ def customerDashboard(request):
 # -----------------------------
 # Dashboard Views
 # -----------------------------
-@login_required(login_url='login')
+@login_required(login_url='loginUser')
 def myaccount(request):
     if request.user.is_authenticated:
         redirecturl = detectUser(request.user)
@@ -168,5 +170,19 @@ def myaccount(request):
         return redirect('loginUser')
     
 
-def activate(request):
-    pass
+def activate(request,uidb64,token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,User.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active = True
+        user.save()
+        messages.success(request,'Account has been activated')
+        return redirect('myaccount')
+    else:
+        messages.error(request,"Invalid activation link")
+        return redirect('myaccount')
+
