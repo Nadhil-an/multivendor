86d47a6b17e55edddee5403df2ee7e9c605bd755
Nadhil-an <nadhil.edu@gmail.com>
2025-09-26 00:40:03 +0530

    latest commit

--- foodonline_main/vendor/views.py
+++ foodonline_main/vendor/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from . models import Vendor
 
 
 # Create your views here.
 
 def vprofile(request):
-    return render(request,'vendor/vprofile.html')
+    vendor = Vendor.objects.get(user=request.user)
+    context = {
+        'vendor':vendor
+    }
+    return render(request,'vendor/vprofile.html',context)
