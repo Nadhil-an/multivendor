fe5bb9976e63f0f4eabd18b5ec8050baddc82ac6
Nadhil-an <nadhil.edu@gmail.com>
2025-09-26 23:26:43 +0530

    vendor dashboard

--- foodonline_main/vendor/forms.py
+++ foodonline_main/vendor/forms.py
@@ -4,8 +4,9 @@ from . models import Vendor
 
 
 class VendorForm(forms.ModelForm):
+    vendor_licence = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
     class Meta:
         model = Vendor
         fields = ['vendor_name','vendor_licence']
 
-    
+     
--- foodonline_main/vendor/views.py
+++ foodonline_main/vendor/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from . models import Vendor
-
+from . forms import VendorForm
+from accounts.models import UserProfile
+from accounts.forms import UserProfileForm
 
 # Create your views here.
 
 def vprofile(request):
-    vendor = Vendor.objects.get(user=request.user)
+    
+    profile = get_object_or_404(UserProfile, user = request.user)
+    vendor_get = get_object_or_404(Vendor, user = request.user)
+
+    profile_form = UserProfileForm(instance=profile)
+    vendor_form = VendorForm(instance=vendor_get)
     context = {
-        'vendor':vendor
+        'profile':profile,
+        'vendor_get':vendor_get,
+        'profile_form':profile_form,
+        'vendor_form':vendor_form
     }
+
+
+    
     return render(request,'vendor/vprofile.html',context)
