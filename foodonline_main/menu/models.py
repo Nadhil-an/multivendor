b99e97fbd3530c6cdea6a5782689e7075da0780f
Nadhil-an <nadhil.edu@gmail.com>
2025-10-02 21:23:52 +0530

    add food items

--- foodonline_main/menu/models.py
+++ foodonline_main/menu/models.py
@@ -26,6 +26,7 @@ class FoodItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='foodimages')
     is_available = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.food_title 
