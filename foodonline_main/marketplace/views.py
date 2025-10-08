1be3066555f5462bc3d8ee9d362b6bf5f024035c
Nadhil-an <nadhil.edu@gmail.com>
2025-10-09 01:23:30 +0530

    setupping market place

--- foodonline_main/marketplace/views.py
+++ foodonline_main/marketplace/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from vendor.models import Vendor
 
 # Create your views here.
 def marketplace(request):
-    return render(request,'marketplace/listing.html')
+    vendors = Vendor.objects.filter(is_approved=True,user__is_active=True)
+    count = vendors.count()
+    context={
+        'vendors':vendors,
+        'count':count
+    }
+    return render(request,'marketplace/listing.html',context)
