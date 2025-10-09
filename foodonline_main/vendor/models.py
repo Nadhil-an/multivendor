190845125bb2bca28ae34d47caaaf9c5fb8378a3
Nadhil-an <nadhil.edu@gmail.com>
2025-10-10 01:53:52 +0530

    auto slug creation

--- foodonline_main/vendor/models.py
+++ foodonline_main/vendor/models.py
@@ -9,7 +9,7 @@ class Vendor(models.Model):
     user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
     user_profile = models.OneToOneField(UserProfile, related_name='userProfile', on_delete=models.CASCADE)
     vendor_name = models.CharField(max_length=50)
-    vendor_slug = models.SlugField(max_length=100, blank=True, null=True)
+    vendor_slug = models.SlugField(max_length=100, unique=True)
     vendor_licence = models.ImageField(upload_to='vendor/licence')
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,28 +18,17 @@ class Vendor(models.Model):
     def __str__(self):
         return self.vendor_name
     def save(self, *args, **kwargs):
-        # Auto-generate slug if it doesn't exist
-        if not self.vendor_slug:
-            self.vendor_slug = slugify(self.vendor_name) + '-' + str(self.user.id)
+            # Auto-generate slug if it doesn't exist
+            if not self.vendor_slug and self.user_id:  # ensure user exists
+                self.vendor_slug = slugify(self.vendor_name) + '-' + str(self.user.id)
 
+            # Send approval email if status changed
+            if self.pk is not None:
+                orig = Vendor.objects.get(pk=self.pk)
+                if orig.is_approved != self.is_approved:
+                    mail_template = 'accounts/emails/vendor_email.html'
+                    context = {'user': self.user, 'is_approved': self.is_approved}
+                    mail_subject = 'Congratulations, Your Restaurant Menu was Approved' if self.is_approved else 'Restaurant Menu was Rejected'
+                    send_approve_mail(mail_template, context, mail_subject)
 
-
-    def save(self, *args, **kwargs):
-        if self.pk is not None:
-            orig = Vendor.objects.get(pk=self.pk)
-            if orig.is_approved != self.is_approved:
-                mail_template = 'accounts/emails/vendor_email.html'
-                context = {
-                    'user': self.user,
-                    'is_approved': self.is_approved,
-                }
-
-                if self.is_approved:
-                    mail_subject = 'Congratulations, Your Restaurant Menu was Approved'
-                else:
-                    mail_subject = 'Restaurant Menu was Rejected'
-
-                send_approve_mail(mail_template, context, mail_subject)
-
-        # Always call super() to save changes
-        super().save(*args, **kwargs)
+            super().save(*args, **kwargs)
