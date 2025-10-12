601007f14215dbed0e07fd1486e7f1b85d1e99a9
Nadhil-an <nadhil.edu@gmail.com>
2025-10-12 23:20:42 +0530

    add to cart context processor

--- foodonline_main/foodonline_main/settings.py
+++ foodonline_main/foodonline_main/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'marketplace.context_processor.get_cart_counter',
             ],
         },
     },
