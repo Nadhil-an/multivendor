163b890189faf44b7e437e6b6e653c741c7cbc61
Nadhil-an <nadhil.edu@gmail.com>
2025-11-15 01:56:50 +0530

    update customer profile page

--- foodonline_main/customers/views.py
+++ foodonline_main/customers/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm,UserInfoForm
-from accounts.models import UserProfile,User
+from accounts.models import UserProfile
+from django.contrib import messages
 # Create your views here.
 @login_required(login_url='login')
 def cprofile(request):
     profile = get_object_or_404(UserProfile,user=request.user)
-
-    userprofile_form = UserProfileForm(instance=profile)
-    user_form = UserInfoForm(instance=request.user)
+    if request.method == 'POST':
+        userprofile_form = UserProfileForm(request.POST,request.FILES, instance=profile)
+        user_form = UserInfoForm(request.POST,instance=request.user)
+        if userprofile_form.is_valid() and user_form.is_valid():
+            userprofile_form.save()
+            user_form.save()
+            messages.success(request,'Profile Updated')
+            return redirect('cprofile')
+        else:
+            print(userprofile_form.errors)
+            print(user_form.errors)
+    else:       
+        userprofile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
 
     context ={
         'user_form':user_form,
         'userprofile_form':userprofile_form,
+        'profile':profile,
         
     }
 
