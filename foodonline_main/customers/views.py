6b705288a443a9026e42a0c673ff56a2b8eb89c8
Nadhil-an <nadhil.edu@gmail.com>
2025-11-15 01:27:40 +0530

    customer dashboard prepopulated form

--- foodonline_main/customers/views.py
+++ foodonline_main/customers/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm,UserInfoForm
+from accounts.models import UserProfile,User
 # Create your views here.
 @login_required(login_url='login')
 def cprofile(request):
-    userprofile_form = UserProfileForm()
-    user_form = UserInfoForm()
+    profile = get_object_or_404(UserProfile,user=request.user)
+
+    userprofile_form = UserProfileForm(instance=profile)
+    user_form = UserInfoForm(instance=request.user)
 
     context ={
         'user_form':user_form,
